49c50b66a18b8687118d82b85a92377235ea8988
BenSerg <75221823+BenSerg@users.noreply.github.com>
2023-10-15 21:53:49 +0300

    Update task_1.py
    Убрал ненужную проверку

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,6 +1,5 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-if None in set(numbers):
-    missing_index = numbers.index(None)
-    numbers[missing_index] = sum(numbers[:missing_index] + numbers[missing_index + 1:]) / len(numbers)
+missing_index = numbers.index(None)
+numbers[missing_index] = sum(numbers[:missing_index] + numbers[missing_index + 1:]) / len(numbers)
 
 print("Измененный список:", numbers)
